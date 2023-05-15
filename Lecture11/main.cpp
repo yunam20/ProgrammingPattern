2f24cd83606967e1832c8a7571bec2cab322afad
yunam20 <zxzxzx00000@naver.com>
2023-05-15 11:39:17 +0900

    10, 11

---
+++ Lecture11/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector> //list¶û ¶È°°À½
+#include <string>
+
+namespace MuSoeun
+{
+	class Scene
+	{
+		int width;
+		int height;
+		std::vector<Object>* ObjList;
+		
+		screenBuf[width + (width * height)];
+		std::cout << screenBuf;
+	};
+
+	class Object
+	{
+		void Render();
+	};
+
+	class Component //Unity MonoBehavior
+	{
+		void Start();
+		void Update();
+	};
+}
+
+int main()
+{
+	while (1)
+	{
+
+	}
+}
