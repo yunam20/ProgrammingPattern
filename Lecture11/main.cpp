a6a6d43b2d1f160357116ba065511015c51f6a28
yunam20 <zxzxzx00000@naver.com>
2023-05-15 11:49:41 +0900

    11

--- Lecture11/main.cpp
+++ Lecture11/main.cpp
@@ -10,8 +10,31 @@ namespace MuSoeun
 		int height;
 		std::vector<Object>* ObjList;
 		
-		screenBuf[width + (width * height)];
-		std::cout << screenBuf;
+		//screenBuf[width + (width * height)];
+		
+		char* screenBuf;
+		void InitScreenBuf()
+		{
+			screenBuf = new char[(width+1) * (height+5)];
+			for (int i; i < height; i++)
+			{
+				screenBuf[(width + 1) + (width + 1) * i] = '\n';
+			}
+			screenBuf[(width + 1) + (width + 1) * (height-1) - 1] = '\n'
+		}
+
+		void ReleaseScreenBuf()
+		{
+			if (screenBuf != nullptr)
+			{
+				delete(screenBuf);
+			}
+		}
+
+		void DrawScreenBuf()
+		{
+			std::cout << screenBuf;
+		}
 	};
 
 	class Object
